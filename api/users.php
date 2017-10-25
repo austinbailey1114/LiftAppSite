a478290399a4ffd5f55f74e9229467484f3ec7bf
Austin Bailey <baileyau@bc.edu>
2017-10-25 09:49:54 -0400

    updated api/users.php to use prepared statements

--- api/users.php
+++ api/users.php
@@ -14,23 +14,28 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
+$user = $_GET['user'];
+
 //user=1 filters out only your lifts
-$sql = "SELECT username FROM users WHERE username = '".$_GET['user']. "'";
+if ($sql = mysqli_prepare($conn, "SELECT username FROM users WHERE username = ?")) {
+	mysqli_stmt_bind_param($sql, 's', $user);
+	mysqli_stmt_execute($sql);
+	$result = mysqli_stmt_store_result($sql);
+	
+	if (mysqli_stmt_num_rows($sql) > 0) {
+		$result = array(
+			'success' => true
+		);
+	} else {
+		$result = array(
+			'success' => false
+		);
+	}
 
-$result = mysqli_query($conn, $sql);
-
-if (mysqli_num_rows($result) > 0) {
-  	$result = array(
-  		'success' => true
-  	);
-} else {
-    $result = array(
-  		'success' => false
-  	);
+	echo json_encode($result);
 }
 
 mysqli_close($conn);
-echo json_encode($result);
 
 
 

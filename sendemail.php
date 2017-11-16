5e00144d108a752f811affdd89a1b8e44a1d8184
Austin Bailey <baileyau@bc.edu>
2017-11-16 14:10:11 -0500

    added random password generator in sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -1,12 +1,25 @@
 <?php
 
+$chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+$newPass = '';
+for ($i=0; $i < 8; $i++) { 
+	$newPass = $newPass . $chars[random_int(0, strlen($chars))];
+}
+
+echo $newPass;
+
 session_start();
 
 $to      = $_SESSION['email'];
 $subject = 'Password Reset';
-$message = 'garbledegook';
-$headers = 'From: liftappsite' . "\r\n" . phpversion();
+$message = 'Your new password is: ' . $newPass;
+$headers = 'From: liftappsite@austinmbailey.com';
 
 mail($to, $subject, $message, $headers);
 
 header('Location: ./settings.php');
+
+
+
+
+

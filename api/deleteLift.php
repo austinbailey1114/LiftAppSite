c63daa354f0619b5c37582ff1af6c04368e58878
Austin Bailey <baileyau@bc.edu>
2017-11-16 17:14:21 -0500

    deleteLift.php now uses getData function

--- api/deleteLift.php
+++ api/deleteLift.php
@@ -1,30 +1,22 @@
 <?php
 
 require '../core/credentials.php';
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-if (!$conn) {
-	die("Connection failed: " . mysqli_connect_error());
-}
+require 'Statement.php';
 
 $id = $_GET['id'];
 
-if ($sql = mysqli_prepare($conn, "DELETE FROM lifts WHERE id = $id")) {
-	mysqli_stmt_bind_param($sql, 'i', $id);
-	mysqli_stmt_execute($sql);
-	$result = mysqli_stmt_store_result($sql);
-	if ($result) {
-		$_SESSION['message'] = 'deleteSuccess';
-		echo "Record deleted successfully";
-	} else {
-		//delete failed
-		echo "Error deleting record: " . mysqli_stmt_error($sql);
-	}
+
+
+$statement = new Statement();
+$result = $statement->deleteData($mysqli, "DELETE FROM lifts WHERE id = $id", $id);
+echo $result;
+if ($result) {
+	$_SESSION['message'] = 'deleteSuccess';
+} else {
+	$_SESSION['message'] = 'deleteFailure';
 }
-mysqli_close($conn);
 header("Location: ../index.php");
 
-?>
 
 
 

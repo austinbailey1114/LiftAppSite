fd3236c079fe3d903591b00fea2a260aa5eeceed
Austin Bailey <baileyau@bc.edu>
2017-11-22 12:21:03 -0500

    now checks for email address and validation of input

--- accountCreate.php
+++ accountCreate.php
@@ -17,6 +17,10 @@
 				<p id="promptName">Name:</p>
 				<input type="text" name="name" id="nameInput" placeholder="name">
 			</div>
+			<div id="emailDiv">
+				<p id="promptEmail">Email:</p>
+				<input type="text" name="email" id="emailInput" placeholder="example@example.com">
+			</div>
 			<div id="usernameDiv">
 				<p id='promptUsername'>Username:</p>
 				<input type="text" name="username" id='usernameInput' placeholder="username">
@@ -31,18 +35,29 @@
 </body>
 <script type="text/javascript">
 	
-	function checkInput(value, id, reset) {
+	function checkUsername(value, id, reset) {
 		var xhttp = new XMLHttpRequest();
 		xhttp.open('POST', './api/users.php?user=' + value, false);
 		xhttp.send();
 		var success = JSON.parse(xhttp.responseText);
 		if (success['success']) {
 			var prompt = document.getElementById(id);
-			prompt.innerHTML = "<img src='./images/warning.png' height='20' width='20' style='margin-right: 5px;'>Username Taken"
+			prompt.innerHTML = "<img src='./images/warning.png' height='20' width='20' style='margin-right: 5px;'>Username Taken";
 		} else {
 			var prompt = document.getElementById(id);
 			prompt.textContent = reset;
 		}
+	}
+
+	function checkEmail(email) {
+		var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    	if (re.test(email)) {
+    		var prompt = document.getElementById('promptEmail');
+    		prompt.textContent = "Email:";
+    	} else {
+    		var prompt = document.getElementById('promptEmail');
+    		prompt.innerHTML = "<img src='./images/warning.png' height='20' width='20' style='margin-right: 5px;'>Invalid Email";
+    	}
 	}
 
 	function arrayContains(username, takenUsernames) {
@@ -50,7 +65,9 @@
 	}
 
 	var usernameInput = document.getElementById('usernameInput');
-	usernameInput.addEventListener('input', function() { checkInput(usernameInput.value, 'promptUsername', 'Username: ')});
+	usernameInput.addEventListener('input', function() { checkUsername(usernameInput.value, 'promptUsername', 'Username: ')});
+	var emailInput = document.getElementById('emailInput');
+	emailInput.addEventListener('blur', function() {checkEmail(emailInput.value)});
 	
 	
 

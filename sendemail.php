922b57ae140a33b1e8719b613836e0b0a998ac71
Austin Bailey <baileyau@bc.edu>
2018-01-04 16:30:09 -0500

    updated so password does reset

--- sendemail.php
+++ sendemail.php
@@ -1,18 +1,27 @@
 <?php
 
+require './core/init.php';
+require './Query/Query.php';
+
 $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 $newPass = '';
 for ($i=0; $i < 8; $i++) { 
 	$newPass = $newPass . $chars[random_int(0, strlen($chars))];
 }
 
-echo $newPass;
+var_dump($mysqli);
+
+$query = new Query($mysqli);
+
+var_dump($query);
+
+$result = $query->table('users')->update(array('password'), array(md5($newPass)))->where('id', '=', $_SESSION['id'])->execute();
 
 session_start();
 
 $to      = $_SESSION['email'];
 $subject = 'Password Reset';
-$message = 'Your new password is: ' . $newPass;
+$message = 'Your new password is: ' . $newPass . ". " . $result;
 $headers = 'From: liftappsite@austinmbailey.com';
 
 mail($to, $subject, $message, $headers);
